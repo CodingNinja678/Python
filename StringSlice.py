575c1722c52cfac52bd66b747fd1e2600f0da77e
CodingNinja678 <90537380+CodingNinja678@users.noreply.github.com>
2021-10-05 22:47:34 +0530

    Create StringSlice.py

---
+++ StringSlice.py
@@ -0,0 +1,9 @@
+s="hello"
+print (s[1])
+print(s[-4])
+#negative values of position of elements be used..opposite numbering
+print (s[1:4])
+#s[i:j] -- s[i] to s[j-1]
+#s[i:] -- s[0] to s[length(s)]
+s=s[0:3]+"p!"
+print (s)
